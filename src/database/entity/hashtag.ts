78651ef00e274fe263579a956fbcbc38f62321ed
ritvij14 <ritvij2001@gmail.com>
2021-03-21 00:14:59 +0530

    minor modifications

--- src/database/entity/hashtag.ts
+++ src/database/entity/hashtag.ts
@@ -3,8 +3,8 @@ import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity({ name: 'hashtags' })
 export class Hashtags extends BaseEntity {
     @PrimaryGeneratedColumn()
-    id!: number;
+    id!: bigint;
 
-    @Column('varchar')
+    @Column({ type: 'varchar', unique: true })
     tag_name!: string;
 }

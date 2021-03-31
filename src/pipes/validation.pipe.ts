a388eed75e5c8b6417d6d0c7977edf92ead10b1d
Shreyash <pandeyshreyash2201@gmail.com>
2021-03-31 20:56:32 +0530

    Added Custom Pipe for validation

---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,24 @@
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+	async transform(value: any, { metatype }: ArgumentMetadata) {
+		if (!metatype || !this.toValidate(metatype)) {
+			return value;
+		}
+		const object = plainToClass(metatype, value);
+		console.log(object);
+		const errors = await validate(object);
+		if (errors.length > 0) {
+			throw new BadRequestException('Validation failed');
+		}
+		return object;
+	}
+
+	private toValidate(metatype: Function): boolean {
+		const types: Function[] = [String, Boolean, Number, Array, Object];
+		return !types.includes(metatype);
+	}
+}
